83ed1032172b8029c4d63b027ec857c3ba848427
abdullah1116 <ahasan1116@gmail.com>
2019-12-29 23:28:20 +0500

    beginning

---
+++ index.js
@@ -0,0 +1,49 @@
+var t1,
+    avgArray = [],
+    imgsrcset;
+
+getResult()
+
+function getResult() {
+    document.getElementById("Imagenoload").style.display = "none"
+    document.getElementById("ImageObject").innerHTML = ""
+    document.getElementById("showResult").textContent = "Wait..."
+    t1 = performance.now()
+    load()
+}
+
+function load() {
+    document.getElementById("ImageObject").data = "image.svg"
+}
+
+function result() {
+    var t2 = performance.now(),
+        timeresult = 0;
+    avgArray.push(t2 - t1)
+    for (i = 0; i < avgArray.length; i++) {
+        timeresult += avgArray[i] / avgArray.length
+    }
+
+    document.getElementById("showResult").textContent = Math.round(timeresult) / 1000 + " Seconds (" + avgArray.length + " avg)"
+    document.getElementById("Result_big").textContent = Math.round(timeresult / 1000)
+    document.getElementById("Result_small").textContent = Math.round(timeresult) / 1000
+
+
+    if (imgsrcset == true) {
+        document.getElementById("Imagenoload").style.display = "block"
+    } else {
+        imgsrcset == true
+        document.getElementById("Imagenoload").src = "image.jpg"
+        document.getElementById("Imagenoload").style.display = "block"
+
+    }
+}
+
+function clearResult() {
+    avgArray = []
+    document.getElementById("showResult").textContent = "Cleared"
+}
+
+function Imageonload() {
+    document.getElementById("ImageObject").data = ""
+}
