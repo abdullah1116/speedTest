70eae372f0a5dcc91f7167853eee17352b67440a
abdullah1116 <ahasan1116@gmail.com>
2020-06-17 21:24:22 +0500

    1.0 fixed

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-load();
-document.getElementById("background").style.display = "none"
 var t1,
     avgArray = [],
     imgsrcset;
 
-setTimeout(
-    function getResult() {
-        document.getElementById("Imagenoload").style.display = "none"
-        document.getElementById("ImageObject").innerHTML = ""
-        document.getElementById("showResult").textContent = "Wait..."
-        t1 = performance.now()
-        load()
-        result()
-    }, 1000);
+function getResult() {
+    document.getElementById("Imagenoload").style.display = "none"
+    document.getElementById("ImageObject").innerHTML = ""
+    document.getElementById("showResult").textContent = "Wait..."
+    t1 = performance.now()
+    load()
+}
 
 function load() {
-    // document.getElementById("ImageObject").data = "image.svg"
     $("#ImageObject")[0].data = "/Image.svg"
-    console.log('$("#ImageObject")[0].data = "/Image.svg"')
 }
 
 function result() {
@@ -28,25 +22,33 @@ function result() {
         timeresult += avgArray[i] / avgArray.length
     }
 
-    document.getElementById("showResult").textContent = Math.round(timeresult) / 1000 + " Seconds (" + avgArray.length + " avg)"
-    document.getElementById("Result").textContent = Math.round(timeresult) / 1000
+    $("#showResult").text(Math.round(timeresult) / 1000 + " Seconds (" + avgArray.length + " avg)")
+    $("#Result").text((Math.round(timeresult) / 1000).toFixed(2))
 
 
-    if (imgsrcset == true) {
-        document.getElementById("Imagenoload").style.display = "block"
-    } else {
-        imgsrcset == true
-        document.getElementById("Imagenoload").src = "image.jpg"
-        document.getElementById("Imagenoload").style.display = "block"
+    // if (imgsrcset == true) {
+    //     document.getElementById("Imagenoload").style.display = "block"
+    // } else {
+    //     imgsrcset == true
+    //     document.getElementById("Imagenoload").src = "image.jpg"
+    //     document.getElementById("Imagenoload").style.display = "block"
 
-    }
+    // }
 }
 
 function clearResult() {
-    avgArray = []
-    document.getElementById("showResult").textContent = "Cleared"
+    avgArray = []    
+    $("#showResult").text("Cleared")
+    $("#Result").text("")
 }
 
 function Imageonload() {
-    // document.getElementById("ImageObject").data = ""
+
+    result()
 }
+function tst() {
+    var DomTreeCopy = $('#ImageObject').clone(true, true);
+    $('#ImageObject').replaceWith(DomTreeCopy);
+}
+
+getResult();
